c1a3789f38a94b21be957f3bea9fd1e24d6fdd59
mccree.li <mccree.li@cloudwise.com>
2022-04-12 19:11:55 +0800

    format

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -11,26 +11,34 @@ impl MigrationName for Migration {
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager.create_table(
-            Table::create()
-            .table(Post::Table)
-            .col(ColumnDef::new(Post::Id).integer().primary_key().auto_increment().not_null())
-            .col(ColumnDef::new(Post::Title).string().not_null())
-            .col(ColumnDef::new(Post::Text).string().not_null())
-            .to_owned()).await
+        manager
+            .create_table(
+                Table::create()
+                    .table(Post::Table)
+                    .col(
+                        ColumnDef::new(Post::Id)
+                            .integer()
+                            .primary_key()
+                            .auto_increment()
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(Post::Title).string().not_null())
+                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .to_owned(),
+            )
+            .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager.drop_table(Table::drop()
-        .table(Post::Table)
-        .to_owned()).await
+        manager
+            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .await
     }
 }
 #[derive(Iden)]
-pub enum Post{
+pub enum Post {
     Table,
     Id,
     Title,
     Text,
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use migration;
 fn main(){
     
 }
